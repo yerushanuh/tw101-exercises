9e995c421a11858f578eab01401843c098c7d32b
yerushanuh <yerushanuh@gmail.com>
2015-07-13 14:27:04 -0400

    Completed Ex 4

--- src/main/java/com/thoughtworks/tw101/exercises/exercise4/Library.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise4/Library.java
@@ -6,12 +6,22 @@ public class Library {
     private String[] books;
     private PrintStream printStream;
 
+    public Library(String[] books) {
+        this(books, System.out);
+    }
+
     public Library(String[] books, PrintStream printStream) {
         this.books = books;
         this.printStream = printStream;
     }
 
     public String printBooksContaining(String partialBookTitle) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].contains(partialBookTitle)) {
+                printStream.println(books[i]);
+            }
+        }
+
         return null;
     }
 }
